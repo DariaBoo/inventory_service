f6aac2e1e2f0cada816e71516645bd482f20d988
Natallia Bulyha <bulyhanatallia@gmail.com>
2023-09-11 20:16:53 +0200

    CH-474/CH-551 added method to check product available and status jira[create method to return dto for shopping cart by product skuCode and colorHex] https://cozyhometeam.atlassian.net/browse/CH-551?atlOrigin=eyJpIjoiNjJhZmUxZmVmZDRhNGVmNzgyMjE5MDQzODQxYzc3MmYiLCJwIjoiaiJ9

---
+++ src/main/java/com/cozyhome/inventory/config/ProductColorDtoSerializer.java
@@ -0,0 +1,23 @@
+package com.cozyhome.inventory.config;
+
+import com.cozyhome.inventory.dto.ProductColorDto;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+@Configuration
+public class ProductColorDtoSerializer extends JsonSerializer<ProductColorDto> {
+
+    private ObjectMapper mapper = new ObjectMapper();
+    @Override
+    public void serialize(ProductColorDto productColorDto, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        StringWriter writer = new StringWriter();
+        mapper.writeValue(writer, productColorDto);
+        jsonGenerator.writeFieldName(writer.toString());
+    }
+}

46b909c8018bab71ac2924a200c5c59f3002bcb7
Natallia Bulyha <bulyhanatallia@gmail.com>
2023-09-11 19:23:27 +0200

    CH-474/CH-551 added method to check product available and status jira[create method to return dto for shopping cart by product skuCode and colorHex] https://cozyhometeam.atlassian.net/browse/CH-551?atlOrigin=eyJpIjoiNjJhZmUxZmVmZDRhNGVmNzgyMjE5MDQzODQxYzc3MmYiLCJwIjoiaiJ9

--- src/main/java/com/cozyhome/inventory/dto/ProductColorDto.java
+++ src/main/java/com/cozyhome/inventory/dto/ProductColorDto.java
@@ -2,14 +2,15 @@ package com.cozyhome.inventory.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@EqualsAndHashCode
 public class ProductColorDto {
 
 	private String productSkuCode;
 	private String colorHex;
-	
 }
